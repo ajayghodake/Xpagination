fd429a2b8f80af2cde88475470c3029c2c161800
Ajay Ghodake <it.ajayghodake@gmail.com>
2025-02-05 22:08:58 +0530

    code updated

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,79 +1,84 @@
-import React from 'react'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import style from './pagination.module.css'
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import style from "./pagination.module.css";
 
 export default function Pagination() {
-    const [userData, setUserData ] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const dataPerPage = 10;
+  const [userData, setUserData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const dataPerPage = 10;
 
-useEffect(()=>{
+  useEffect(() => {
     const fetchData = async () => {
-        try{
-            const res = await axios.get("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
-            const data = res.data;
-            setUserData(data);
-        }catch(error) {
-            alert("Failed to fetch data");
-            console.error("failed to fetch data");
-        }
-    }
+      try {
+        const res = await axios.get(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        const data = res.data;
+        setUserData(data);
+      } catch (error) {
+        alert("Failed to fetch data");
+        console.error("failed to fetch data");
+      }
+    };
 
     fetchData();
-}, [])
+  }, []);
 
+  const totalPages = Math.ceil(userData.length / dataPerPage);
+  const startIndex = (currentPage - 1) * dataPerPage;
+  const endIndex = startIndex + dataPerPage;
+  const displayedData = userData.slice(startIndex, endIndex);
 
-
-const totalPages = Math.ceil(userData.length / dataPerPage);
-const startIndex =  (currentPage - 1) * dataPerPage;
-const endIndex = startIndex + dataPerPage;
-const displayedData =  userData.slice(startIndex, endIndex);
-
-const handleNext = () => {
-    if(currentPage < totalPages){
-        setCurrentPage((prev) => prev + 1);
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prev) => prev + 1);
     }
-}
+  };
 
-const handlePrevious = () => {
-    if(currentPage > 1) {
-        setCurrentPage(prev => prev - 1)
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
     }
-}
+  };
 
   return (
     <>
-     <div className={style.container}>
+      <div className={style.container}>
         <h2>Employee Data Table</h2>
         <table>
-            <thead>
-                <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>EMAIL</th>
-                <th>ROLE</th>
-                </tr>
-            </thead>
-            <tbody>
-                {displayedData.map((data)=> (
-                    <tr key={data.id}>
-                    <td>{data.id}</td>
-                    <td>{data.name}</td>
-                    <td>{data.email}</td>
-                    <td>{data.role}</td>
-                    </tr>
-                ))}
-            </tbody>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ROLE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {displayedData.map((data) => (
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+                <td>{data.role}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        <div className="buttonContainer">
-        <button onClick={handlePrevious} disabled={currentPage===1}>Previous</button>
-        <button >{currentPage}</button>
-        {/* <span> Page {currentPage} of {totalPages} </span> */}
-        <button onClick={handleNext} disabled={currentPage===totalPages}>Next</button>
+        <div className={style.buttonContainer}>
+          <button onClick={handlePrevious} disabled={currentPage === 1}>
+            Previous
+          </button>
+          <div className={style.pageNo}>
+            {currentPage}
+          </div>
+          {/* <span> Page {currentPage} of {totalPages} </span> */}
+          <button onClick={handleNext} disabled={currentPage === totalPages}>
+            Next
+          </button>
         </div>
-        
-     </div>
+      </div>
     </>
-  )
+  );
 }
